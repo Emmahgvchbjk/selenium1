46dd6596f1b7b90c857f8ea4f20c0d2cfdc1fea5
Emmahgvchbjk <96427598+Emmahgvchbjk@users.noreply.github.com>
2022-02-15 16:20:35 +0200

    selenium tests

--- tests/book_flight_test.py
+++ tests/book_flight_test.py
@@ -18,22 +18,52 @@ def test_package():
     location_bar.send_keys("SFO")
     destination_bar = driver.find_element(By.CSS_SELECTOR, "input[data-bdd='farefinder-package-destination-location-input']")
     destination_bar.send_keys("LAX")
-    bundles_button.click()
+    driver.find_element(By.CSS_SELECTOR, "div.farefinder-container").click()
 
-    time.sleep(5)
-    date_tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    date_of_departure_selector = date_tomorrow.strftime("%B") + " " + date_tomorrow.strftime("%d") + ", " + date_tomorrow.strftime("%Y")
-    data_from_departure_selector= 'td[aria-label=\"'+ date_of_departure_selector +'\"]'
-
-    date_of_return = date_tomorrow + datetime.timedelta(days=20)
-    date_of_return_selector = date_of_return.strftime("%B") + " " + date_of_return.strftime("%d") + ", " + date_tomorrow.strftime("%Y")
-    data_from_returning_selector ='td[aria-label=\"'+ date_of_return_selector +'\"]'
-
-    departing_date = driver.find_element(By.CSS_SELECTOR, "div[data-bdd ='farefinder-package-startdate-input']")
+    # time.sleep(5)
+    # date_tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    # # date_of_departure_selector = date_tomorrow.strftime("%B") + " " + date_tomorrow.strftime("%d") + ", " + date_tomorrow.strftime("%Y")
+    # data_from_departure_selector= 'td[aria-label='+ date_of_departure_selector +']'
+    #
+    # date_of_return = date_tomorrow + datetime.timedelta(days=20)
+    # # date_of_return_selector = date_of_return.strftime("%B") + " " + date_of_return.strftime("%d") + ", " + date_tomorrow.strftime("%Y")
+    # data_from_returning_selector ='td[aria-label='+ date_of_return_selector +']'
+    #
+    # next_day_detailed = date_tomorrow.strftime('%B %d, %Y')
+    # if next_day_detailed[-8] == "0":
+    #     next_day_detailed = next_day_detailed.replace(next_day_detailed[-8], "", 1)
+    # print("conversion of next day", next_day_detailed)
+    # next_day20_detailed = date_of_return.strftime('%B %d, %Y')
+    # if next_day20_detailed[-8] == "0":
+    #     next_day20_detailed = next_day20_detailed.replace(next_day20_detailed[-8], "", 1)
+    #
+    # departing_date = driver.find_element(By.CSS_SELECTOR, "div[data-bdd ='farefinder-package-startdate-input']")
+    # departing_date.click()
+    # destination_bar.send_keys(Keys.PAGE_DOWN)
+    # select_departing_date = driver.find_element(By.CSS_SELECTOR, data_from_departure_selector)
+    # select_departing_date.click()
+    #
+    # select_returning_date = driver.find_element(By.CSS_SELECTOR, data_from_returning_selector)
+    # select_returning_date.click()
+    next_day = datetime.datetime.today() + datetime.timedelta(days=1)
+    next_day20 = datetime.datetime.today() + datetime.timedelta(days=20)
+    next_day_detailed = next_day.strftime('%B %d, %Y')
+    if next_day_detailed[-8] == "0":
+        next_day_detailed = next_day_detailed.replace(next_day_detailed[-8], "", 1)
+    print("conversion of next day", next_day_detailed)
+    next_day20_detailed = next_day20.strftime('%B %d, %Y')
+    if next_day20_detailed[-8] == "0":
+        next_day20_detailed = next_day20_detailed.replace(next_day20_detailed[-8], "", 1)
+    print("conversion of 20+ day", next_day20_detailed)
+    date_from_selector = "td[aria-label*=\""+next_day_detailed+"\"]"
+    date_to_selector = "td[aria-label*=\""+next_day20_detailed+"\"]"
+    departing_date = driver.find_element(By.CSS_SELECTOR, '[data-bdd="farefinder-package-startdate-input"]')
     departing_date.click()
-    destination_bar.send_keys(Keys.PAGE_DOWN)
-    select_departing_date = driver.find_element(By.CSS_SELECTOR, data_from_departure_selector)
-    select_departing_date.click()
-
-    select_returning_date = driver.find_element(By.CSS_SELECTOR, data_from_returning_selector)
-    select_returning_date.click()
+    date_from = driver.find_element(By.CSS_SELECTOR, date_from_selector)
+    date_from.click()
+    date_to = driver.find_element(By.CSS_SELECTOR, date_to_selector)
+    date_to.click()
+    time.sleep(5)
+    find_deals_button = driver.find_element(By.CSS_SELECTOR, "[data-bdd='farefinder-package-search-button']")
+    find_deals_button.click()
+    time.sleep(5)
